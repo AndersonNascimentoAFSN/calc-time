7154e068e2330d82a01b544d04595e0a2c75f10d
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-04-25 19:12:24 -0300

    refactor: move text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@ import { Conversor } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-6 p-24">
-      <h1 className="text-5xl font-bold">Conversor</h1>
-
+    <main>
       <Conversor />
     </main>
   );
--- src/components/conversor/conversor.tsx
+++ src/components/conversor/conversor.tsx
@@ -40,58 +40,62 @@ export function Conversor() {
   const decimalTime = convertDecimalToTime()
 
   return (
-    <Tabs defaultValue="time-decimal" className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="time-decimal">Tempo-Decimal</TabsTrigger>
-        <TabsTrigger value="decimal-time">Decimal-Tempo</TabsTrigger>
-      </TabsList>
-      <TabsContent value="time-decimal">
-        <Card>
-          <CardHeader>
-            <CardTitle>Tempo Decimal</CardTitle>
-            <CardDescription>
-              Converta o tempo em formato de string 00:00 para decimal.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="time">Tempo</Label>
-              <Input id="time" defaultValue="00:00" placeholder="00:00" onChange={(e: any) => setTime(e.target.value)} />
-            </div>
-          </CardContent>
+    <div className="flex min-h-screen flex-col items-center gap-6 p-24">
+      <h1 className="text-5xl font-bold">Conversões</h1>
 
-          <CardFooter>
-            {/* <Button>Converter</Button> */}
-            Valor convertido: {timeDecimal}
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="decimal-time">
-        <Card>
-          <CardHeader>
-            <CardTitle>Decimal Time</CardTitle>
-            <CardDescription>
-              Converta um decimal para o formato de tempo.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="decimal">Decimal</Label>
-              <Input id="decimal" type="number" defaultValue={0.000} onChange={(e: any) => setDecimal(Number(e.target.value))} />
-            </div>
-          </CardContent>
-
-          <CardFooter>
-            <div>
+      <Tabs defaultValue="time-decimal" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="time-decimal">Tempo-Decimal</TabsTrigger>
+          <TabsTrigger value="decimal-time">Decimal-Tempo</TabsTrigger>
+        </TabsList>
+        <TabsContent value="time-decimal">
+          <Card>
+            <CardHeader>
+              <CardTitle>Tempo Decimal</CardTitle>
+              <CardDescription>
+                Converta o tempo em formato de string 00:00 para decimal.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
               <div className="space-y-1">
-                Valor convertido: {decimalTime}
+                <Label htmlFor="time">Tempo</Label>
+                <Input id="time" defaultValue="00:00" placeholder="00:00" onChange={(e: any) => setTime(e.target.value)} />
               </div>
+            </CardContent>
 
-              {/* <Button className='mt-2'>Converter</Button> */}
-            </div>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-    </Tabs>
+            <CardFooter>
+              {/* <Button>Converter</Button> */}
+              Valor convertido: {timeDecimal}
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="decimal-time">
+          <Card>
+            <CardHeader>
+              <CardTitle>Decimal Time</CardTitle>
+              <CardDescription>
+                Converta um decimal para o formato de tempo.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="decimal">Decimal</Label>
+                <Input id="decimal" type="number" defaultValue={0.000} onChange={(e: any) => setDecimal(Number(e.target.value))} />
+              </div>
+            </CardContent>
+
+            <CardFooter>
+              <div>
+                <div className="space-y-1">
+                  Valor convertido: {decimalTime}
+                </div>
+
+                {/* <Button className='mt-2'>Converter</Button> */}
+              </div>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
   )
 }
