460ee451774e728b971a609b1f9ee7b1c73653d1
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-04-26 16:29:02 -0300

    feat: test time

--- src/components/current-date/currentDate.tsx
+++ src/components/current-date/currentDate.tsx
@@ -4,7 +4,8 @@ import { useEffect } from "react"
 import { createDateInTimeZone } from "@/utils/create-date-in-time-zone"
 
 export function CurrentDate() {
-  const currentDate = new Date()
+  const now = new Date()
+
   const currentDateFormat = Intl.DateTimeFormat('pt-BR', {
     year: 'numeric',
     month: 'long',
@@ -13,18 +14,18 @@ export function CurrentDate() {
     minute: '2-digit',
     second: '2-digit',
     hour12: false
-  }).format(currentDate)
+  }).format(now)
 
-  const dateInTimeZoneBr = createDateInTimeZone(currentDate, -3)
-  const dateInTimeZoneBrFormat = Intl.DateTimeFormat('pt-BR', {
-    year: 'numeric',
-    month: 'long',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false
-  }).format(dateInTimeZoneBr)
+  // const dateInTimeZoneBr = createDateInTimeZone(currentDate, -3)
+  // const dateInTimeZoneBrFormat = Intl.DateTimeFormat('pt-BR', {
+  //   year: 'numeric',
+  //   month: 'long',
+  //   day: '2-digit',
+  //   hour: '2-digit',
+  //   minute: '2-digit',
+  //   second: '2-digit',
+  //   hour12: false
+  // }).format(dateInTimeZoneBr)
 
   useEffect(() => {
     const getIp = async () => {
@@ -37,25 +38,34 @@ export function CurrentDate() {
 
   const deadlineDay = process.env.NEXT_PUBLIC_MANAGER_APPROVE_REPPROVE_COMPETENCE_DEADLINE_DAY
   const deadlineHour = process.env.NEXT_PUBLIC_MANAGER_APPROVE_REPPROVE_COMPETENCE_DEADLINE_HOUR
-  console.log(deadlineDay, deadlineHour)
 
-  const currentDateToDeadLine = new Date()
-  const deadLineDate = new Date(currentDateToDeadLine.getFullYear(), currentDateToDeadLine.getMonth(), currentDateToDeadLine.getDate(), Number(deadlineHour?.split(':')[0]), Number(deadlineHour?.split(':')[1]), 0)
+  const deadLineDate = new Date(now.getFullYear(), now.getMonth(), Number(deadlineDay), Number(deadlineHour?.split(':')[0]), Number(deadlineHour?.split(':')[1]), 0)
+
+  const deadLineDateFormat = Intl.DateTimeFormat('pt-BR', {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  }).format(deadLineDate)
+
+  const currentDate = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds())
+  // currentDate.setHours(currentDate.getHours() - 3)
+
+  const isGreater = currentDate > deadLineDate
 
   console.log('deadLineDate', deadLineDate)
+  console.log('currentDate', currentDate)
+  console.log('currentDate > deadLineDate', isGreater)
 
   return (
     <div>
-      <div>
-        <span>Horário Atual (Hospedagem):  <span>{currentDateFormat}</span></span>
-
-        <div>{JSON.stringify(currentDate)}</div>
-      </div>
-
-      <div>
-        <span>Horário Atual (Horário Brasilia):  <span>{dateInTimeZoneBrFormat}</span></span>
-
-        <div>{JSON.stringify(dateInTimeZoneBr)}</div>
+      <div className="flex flex-col gap-4">
+        <span>Dia Atual:  <span>{currentDateFormat}</span></span>
+        <span>Dia de fechamento:  <span>{deadLineDateFormat}</span></span>
+        <span>Dia Atual é maior que dia de fechamento: <span>{String(isGreater)}</span></span>
       </div>
     </div>
   )
