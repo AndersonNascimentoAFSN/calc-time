14c307393f7a864924b71e79e6fbf24c62fccfc6
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-04-26 20:18:51 -0300

    fix: hours to UTC -3 when date is set in the server-side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { createDateInSaoPaulo } from "@/utils";
 export default function Home() {
   const now = new Date()
 
-  const deadlineDay = process.env.NEXT_PUBLIC_MANAGER_APPROVE_REPPROVE_COMPETENCE_DEADLINE_DAY
-  const deadlineHour = process.env.NEXT_PUBLIC_MANAGER_APPROVE_REPPROVE_COMPETENCE_DEADLINE_HOUR
+  const deadlineDay = process.env.MANAGER_APPROVE_REPPROVE_COMPETENCE_DEADLINE_DAY
+  const deadlineHour = process.env.MANAGER_APPROVE_REPPROVE_COMPETENCE_DEADLINE_HOUR
 
   // const deadLineDate = new Date(now.getFullYear(), now.getMonth(), Number(deadlineDay), Number(deadlineHour?.split(':')[0]), Number(deadlineHour?.split(':')[1]), 0)
 
