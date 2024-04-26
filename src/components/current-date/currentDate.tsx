3206cdf470871bb6f41e5a6e45e00aea85e8dfa2
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-04-26 15:46:11 -0300

    feat: test time

--- src/components/current-date/currentDate.tsx
+++ src/components/current-date/currentDate.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useEffect } from "react"
 import { createDateInTimeZone } from "@/utils/create-date-in-time-zone"
 
 export function CurrentDate() {
@@ -22,6 +25,24 @@ export function CurrentDate() {
     second: '2-digit',
     hour12: false
   }).format(dateInTimeZoneBr)
+
+  useEffect(() => {
+    const getIp = async () => {
+      const response = await fetch('https://api.ipify.org?format=json')
+      const ip = await response.json()
+      console.log('ip', ip)
+    }
+    getIp()
+  }, [])
+
+  const deadlineDay = process.env.NEXT_PUBLIC_MANAGER_APPROVE_REPPROVE_COMPETENCE_DEADLINE_DAY
+  const deadlineHour = process.env.NEXT_PUBLIC_MANAGER_APPROVE_REPPROVE_COMPETENCE_DEADLINE_HOUR
+  console.log(deadlineDay, deadlineHour)
+
+  const currentDateToDeadLine = new Date()
+  const deadLineDate = new Date(currentDateToDeadLine.getFullYear(), currentDateToDeadLine.getMonth(), currentDateToDeadLine.getDate(), Number(deadlineHour?.split(':')[0]), Number(deadlineHour?.split(':')[1]), 0)
+
+  console.log('deadLineDate', deadLineDate)
 
   return (
     <div>
